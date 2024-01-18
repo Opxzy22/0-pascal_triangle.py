3e866fc3f482411c6373af016f00860a03ff7b91
opxzy22 <aopeyem84@gmail.com>
2024-01-18 12:34:46 +0100

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def minOPerations(n):
+    #!/usr/bin/python3
+"""
+Defines a function minOperations
+"""
+
+
+def minOperations(n):
+    """
+        Returns a integer of the minimum operation that can occur.
+
+    Args:
+        n (integer): the number of times an operation is made to reach this
+        integer.
+    """
+    if n < 2:
+        return 0
+
+    ops = 0
+    for i in range(2, int(n ** 0.5) + 1):
+        while n % i == 0:
+            ops += i
+            n //= i
+
+    if n > 1:
+        ops = ops + n
+
+    return ops
