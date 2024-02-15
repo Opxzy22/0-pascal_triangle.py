6c3d8d38b767b421d88202eff24dc75c67b95cd1
opxzy22 <aopeyem84@gmail.com>
2024-02-15 15:18:03 -0800

    a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,24 @@
-#!/usr/bin/Node
+#!/usr/bin/node
 const request = require('request');
 
-function fetchData(movieId) {
-    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+function fetchData (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-    request(url, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const films = JSON.parse(body);
-            const characters = films.characters;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const films = JSON.parse(body);
+      const characters = films.characters;
 
-            characters.forEach(charactersURL => {
-                request(charactersURL, function(error, response, body) {
-                    if (!error && response.statusCode === 200) {
-                        const character = JSON.parse(body);
-                        console.log(character.name);
-                    }
-                });
-            });
-        }
-    });
+      characters.forEach(charactersURL => {
+        request(charactersURL, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            const character = JSON.parse(body);
+            console.log(character.name);
+          }
+        });
+      });
+    }
+  });
 }
 const movieId = process.argv[2];
 fetchData(movieId);
-
