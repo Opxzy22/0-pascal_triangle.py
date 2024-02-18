d28942e64680935fd61a5b9558f825f514245be3
opxzy22 <aopeyem84@gmail.com>
2024-02-18 12:13:15 -0800

    fetching names of characters in a film

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,25 +6,25 @@ function fetchData (movieId) {
 
   request(url, async function (error, response, body) {
     if (error) {
-      console.log(error)
-    } else (response.statusCode == 200) {
-      const films = JSON.parse(body)
-      const characters = films.characters
+      console.log(error);
+    } else {
+      const films = JSON.parse(body);
+      const characters = films.characters;
 
-      for (character in characters) {
-        res = await new Promise((resolve, reject) => {
+      for (const character of characters) {
+        const res = await new Promise((resolve, reject) => {
           request(character, (error, response, body) => {
             if (error) {
-              reject(error)
-            } else (response.statusCode == 200) {
+              reject(error);
+            } else {
               resolve(JSON.parse(body));
-              console.log(res.name)
             }
-          })
+          });
         });
+        console.log(res.name);
       }
     }
   });
-};
+}
 const movieId = process.argv[2];
 fetchData(movieId);
