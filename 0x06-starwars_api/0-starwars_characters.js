9e98d5583317a2c407c3ee2c6b79b3ae68bafbf9
opxzy22 <aopeyem84@gmail.com>
2024-02-18 20:23:04 +0100

    work

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,13 +6,25 @@ function fetchData (movieId) {
 
   request(url, async function (error, response, body) {
     if (error) {
-	    console.log(error)
+      console.log(error)
     } else (response.statusCode == 200) {
-	    const film = JSON.parse(body)
-	    const characters = film.characters
-        for (character in characters) {
-	    const res = await new Promise((resolve, reject) => {
+      const films = JSON.parse(body)
+      const characters = films.characters
 
-}
+      for (character in characters) {
+        res = await new Promise((resolve, reject) => {
+          request(character, (error, response, body) => {
+            if (error) {
+              reject(error)
+            } else (response.statusCode == 200) {
+              resolve(JSON.parse(body));
+              console.log(res.name)
+            }
+          })
+        });
+      }
+    }
+  });
+};
 const movieId = process.argv[2];
 fetchData(movieId);
